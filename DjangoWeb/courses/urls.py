bc6667a7bea6d45396ae428231a96e199e755f54
Natiphon Chanphet <6410615030@cn103>
2022-09-15 09:43:17 +0700

    PangStat

--- DjangoWeb/courses/urls.py
+++ DjangoWeb/courses/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= "index"),
-    path("<int:id>", views.course, name="course"),
-    path("<int:id>/book", views.book, name="book"),
+    path("<int:id>/",views.course, name="course"),
+    path("<int:id>/book",views.book, name="book"),
 ]
