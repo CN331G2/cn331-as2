beab9b918c014b1dd866326ddda05efcdb7fd4c3
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-09-20 21:59:08 +0700

    allin

--- DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://cn331as3herokuapp.herokuapp.com/']
+
+import django_heroku
+django_heroku.settings(locals())
