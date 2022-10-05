067d86bd8c3e64cba16a7e6a4bf6129fd5d40247
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-10-06 00:31:57 +0700

    test update 12:31

--- DjangoWeb/courses/models.py
+++ DjangoWeb/courses/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -10,16 +11,10 @@ class Course(models.Model):
     semmester = models.IntegerField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     seat_count = models.IntegerField(default=0, null=True, blank=True)
-    max_seat = models.IntegerField(null=True, blank=True)
+    max_seat = models.IntegerField(null=True, blank=True, default=5)
     quota = models.BooleanField(default=True)
     attend = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return f"code:{self.c_id} title:{self.title} semmester:{self.semmester} year:{self.year} seat count:{self.seat_count} seat max:{self.max_seat}quota:{self.quota}"
 
-# class Attendance(models.Model):
-#     s_id = models.CharField(max_length=10, default="-")
-#     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True, related_name="deleteuser")
-    
-#     def __str__(self):
-#         return f"{self.id} s_id:{self.s_id} user:{self.user}"
--- DjangoWeb/courses/tests.py
+++ DjangoWeb/courses/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
+from django.urls import reverse
+from django.contrib.auth.hashers import make_password
+from setuptools import setup
+import unittest
+
+from courses.views import book
+from .models import Course
 # Create your tests here.
+
+class CourseTestCase(TestCase):
+
+    def setup(self):
+
+        self.client = Client()
+
+        password_k = make_password('kritpassword')
+        password_p = make_password('pangpassword')
+
+        user_k = User.objects.create(username='krit', password=password_k)
+        user_p = User.objects.create(username='pang', password=password_p)
+
+        Course.objects.create(c_id='test_c1')
+        Course.objects.create(c_id='test_c2', max_seat=1)
+
+    def test_registration_view(self):
+        self.client.login(username='krit', password='kritpassword')
+
+        response = self.client.get(reverse('c_index'))
+        self.assertEqual(response.status_code, 200)
