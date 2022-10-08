91c29cf07b207d43f267d12ee01c94560153a8f4
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-10-08 21:55:32 +0700

    change dirag

--- DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CSRF_TRUSTED_ORIGINS = []
 
-CSRF_TRUSTED_ORIGINS = ['https://stormy-ravine-24433.herokuapp.com/login/']
+CSRF_TRUSTED_ORIGINS = ['https://kittpangcn331as3.herokuapp.com/login/']
 
 import django_heroku
 
